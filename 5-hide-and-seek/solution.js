08c91c6eaaa0b2ac4761b7baf729d549eb9ac2db
Hugh Burgess <hughburgessgermany@hotmail.com>
2021-03-17 15:55:51 +0100

    6 done

--- 5-hide-and-seek/solution.js
+++ 5-hide-and-seek/solution.js
@@ -3,4 +3,9 @@ let box = document.querySelector(".box");
 
 button.addEventListener("click", () => {
   box.classList.toggle("hidden");
+  if (button.textContent === "Hide") {
+    button.textContent = "Show";
+  } else {
+    button.textContent = "Hide";
+  }
 });
