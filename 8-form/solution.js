e88b28b2b66f03f1d9bdb78af4ee90256d4ce1d7
Hugh Burgess <hughburgessgermany@hotmail.com>
2021-03-18 12:08:06 +0100

    8 done

--- 8-form/solution.js
+++ 8-form/solution.js
@@ -1,1 +1,29 @@
 const data = [];
+
+let form = document.querySelector(".super-form");
+
+form.addEventListener("submit", (event) => {
+  if (document.getElementById("married") === "on") {
+    document.getElementById("married").value = "checked";
+  }
+  event.preventDefault();
+
+  const inputForName = form["name"];
+  const inputForAge = form["age"];
+  const inputForMarried = form["married"];
+  const inputForHobbies = form["hobbies"];
+
+  // object
+  const object = {
+    name: inputForName.value,
+    age: inputForAge.value,
+    married: inputForMarried.value,
+    hobbies: inputForHobbies.value,
+  };
+
+  // push object into array called Data
+  data.push(object);
+  console.log(data);
+
+  form.reset();
+});
